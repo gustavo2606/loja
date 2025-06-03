58a0fe4b95a34c46bcc4718b54a5f1aa3a16b9c8
Gustavo Ribeiro Serafim <gustavoserafim59@gmail.com>
2025-06-02 22:30:28 -0300

    recorte das partes

---
+++ includes/rodape.php
@@ -0,0 +1,5 @@
+<footer class="rodape">
+        <p class="texto-rodape">2025 &copy; Desenvolvido por <strong>[Gustavo]</strong> | Projeto fictício sem fins comerciais.</p>
+        <small><?php echo date ("Y"); ?></small>
+    </footer>
+   
--- index.php
+++ index.php
@@ -1,59 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>METEORA</title>
-    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <header class="cabecalho">
-        <nav class="menu">
-            <img src="images/logo.png" alt="METEORA" class="logo">
-            <a href="home.html" class="link-menu ativo">Home</a>
-            <a href="lojas.html" class="link-menu">Lojas</a>
-            <a href="novidades.html" class="link-menu">Novidades</a>
-            <a href="promocoes.html" class="link-menu">Promoções</a>
-        </nav>
-        <form class="pesquisa">
-            <input type="text" class="campo-texto" placeholder="Digite o produto" maxlength="50" minlength="5" required>
-            <button class="botao">Buscar</button>
-        </form>
-    </header>
-    <div class="banner">
-        <img src="images/banner.png" alt="banner meteora" class="imagem-banner">
-    </div>
-    <div class="categorias">
-        <h2 class="titulo">Busque por categoria:</h2>
-        <nav class="menu-categorias">
-            <a href="#" class="item-categoria">
-                <img src="images/categoria-camisetas.png" class="foto-categoria" alt="categoria de camisetas">
-                <p class="texto-categoria">Camisetas</p>
-            </a>
-            <a href="#" class="item-categoria">
-                <img src="images/categoria-bolsas.png" class="foto-categoria" alt="categoria de bolsas">
-                <p class="texto-categoria">Bolsas</p>
-            </a>
-            <a href="#" class="item-categoria">
-                <img src="images/categoria-calcados.png" class="foto-categoria" alt="categoria de calçados">
-                <p class="texto-categoria">Calçados</p>
-            </a>
-            <a href="#" class="item-categoria">
-                <img src="images/categoria-calcas.png" class="foto-categoria" alt="categoria de calças">
-                <p class="texto-categoria">Calças</p>
-            </a>
-            <a href="#" class="item-categoria">
-                <img src="images/categoria-casacos.png" class="foto-categoria" alt="categoria de casacos">
-                <p class="texto-categoria">Casacos</p>
-            </a>
-            <a href="#" class="item-categoria">
-                <img src="images/categoria-oculos.png" class="foto-categoria" alt="categoria de óculos">
-                <p class="texto-categoria">óculos</p>
-            </a>
-        </nav>
-    </div>
+<?php include "includes/cabecalho.php"; ?>
+<?php include "includes/banner.php"; ?>
+<?php include "includes/categorias.php"; ?>
 
+   
+   
     <div class="produtos">
         <h2 class="titulo">Produtos que estão bombando!</h2>
         <div class="lista-produtos">
@@ -102,44 +52,15 @@
         </div>
     </div>
 
-    <div class="facilidades">
-        <h2 class="titulo-facilidades">Conheça todas as nossas facilidades</h2>
-        <div class="icones">
-            <div class="conjunto-icone">
-                <img src="images/icone-pix.png" class="icone">
-                <div class="informacoes-icone">
-                    <h4 class="titulo-icone">PAGUE PELO PIX</h4>
-                    <p class="texto-icone">Ganhe 5% OFF em pagamentos via PIX</p>
-                </div>
-            </div>
-            <div class="conjunto-icone">
-                <img src="images/icone-troca.png" class="icone">
-                <div class="informacoes-icone">
-                    <h4 class="titulo-icone">TROCA GRÁTIS</h4>
-                    <p class="texto-icone">Fique livre para trocar em até 30 dias.</p>
-                </div>
-            </div>
-            <div class="conjunto-icone">
-                <img src="images/icone-sustentabilidade.png" class="icone">
-                <div class="informacoes-icone">
-                    <h4 class="titulo-icone">SUSTENTABILIDADE</h4>
-                    <p class="texto-icone">Moda responsável, que respeita o meio ambiente.</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php include "includes/facilidades.php"; ?>
+    <?php include "includes/newsletter.php"; ?>
+    <?php include "includes/rodape.php"; ?>
 
-    <div class="promocoes">
-        <h3 class="titulo-promocoes">Quer receber nossas novidades, promoções exclusivas e 10% OFF na primeira compra? Cadastre-se!</h3>
-        <form class="formulario-promocoes">
-            <input type="text" class="campo-promocoes" placeholder="Digite seu email">
-            <button type="button" class="botao-promocoes">Enviar</button>
-        </form>
-    </div>
+    
 
-    <footer class="rodape">
-        <p class="texto-rodape">2025 &copy; Desenvolvido por <strong>[Guto Xavier]</strong> | Projeto fictício sem fins comerciais.</p>
-    </footer>
+   
+
+   
 
     <script src="app.js"></script>
 </body>
